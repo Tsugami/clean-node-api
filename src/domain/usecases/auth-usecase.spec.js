4529d173490e9f5edb0f99f9c00af326ab31b3b6
Yslan Ramos <yslan3000@gmail.com>
2021-02-09 01:01:35 -0300

    ensure AuthUseCase throws MissingParamError if no password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,7 +1,8 @@
 const { MissingParamError } = require('../../utils/errors')
 class AuthUseCase {
-  async auth (email) {
+  async auth (email, password) {
     if (!email) throw new MissingParamError('email')
+    if (!password) throw new MissingParamError('password')
   }
 }
 
@@ -10,5 +11,11 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCase()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
+  })
+
+  test('Should return null if no password is provided', async () => {
+    const sut = new AuthUseCase()
+    const promise = sut.auth('any_email@mail.com')
+    expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 })
