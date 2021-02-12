ee450977a29b94726324c6b8b45673465c631e68
Yslan Ramos <yslan3000@gmail.com>
2021-02-12 01:40:55 -0300

    simplify code with factory helpers

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -13,8 +13,8 @@ class AuthUseCase {
     const user = await this.loadUserByEmailRepository.load(email)
     if (!user) return null
 
-    await this.encrypter.compare(password, user.password)
-    return null
+    const isValid = await this.encrypter.compare(password, user.password)
+    if (!isValid) return null
   }
 }
 
